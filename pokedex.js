debbf8fb5ccb31ef3264eacbac772a7e3693c0f0
Margi Africano <africanomargi@gmail.com>
2022-11-07 17:55:58 -0500

    Pokedex

---
+++ pokedex.js
@@ -0,0 +1,27 @@
+const fetchPokemon = async () => {
+    const pokeNameInput = document.getElementById("pokeName");
+    let pokeName = pokeNameInput.value;
+    pokeName = pokeName.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+    let data = await fetch(url).then((res) => {
+        if (res.status != "200") {
+            console.log(res);
+            pokeImage("./img/pokemon-sad.gif")
+        }
+        else {
+            return res.json();
+        }
+    })
+    
+    if (data) {
+        console.log(data);
+        let pokeImg = data.sprites.front_default;
+        pokeImage(pokeImg);
+        console.log(pokeImg);
+    }
+}
+
+const pokeImage = (url) => {
+    const pokePhoto = document.getElementById("pokeImg");
+    pokePhoto.src = url;
+}
